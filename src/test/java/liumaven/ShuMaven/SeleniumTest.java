cc99765bf97b1efd2a0a16daa9a51ed3c442384e
shu <sliu17629@gmail.com>
2020-12-29 14:50:05 -0500

    from Stuff dev

--- src/test/java/liumaven/ShuMaven/SeleniumTest.java
+++ src/test/java/liumaven/ShuMaven/SeleniumTest.java
@@ -5,11 +5,13 @@ public class SeleniumTest {
 	
 	@Test
 	public void browserAutomation(){
-		
 		System.out.println("browserAutomation");
 		System.out.println("browserAutomation2");
 		System.out.println("browserAutomation3");
-		System.out.println("browserAutomation4");
-		System.out.println("browserAutomation5");
+		
+		System.out.println("browserAutomation-a");
+		System.out.println("browserAutomation-b");
+		System.out.println("browserAutomation-c");
+
 	}
 }
